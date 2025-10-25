1ef5db4cdf45d5aad7eddbaabdf29ffba4da064b
gaurav <gauravanurag36@gmail.com>
2025-10-25 13:43:43 +0530

    feat: update registroy/index.json and udpate app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import {
-  FloatingNavbar,
-  GlassNavbar,
-  MorphingNavbar,
-} from "@/components/bitsandpieces/navbars";
+import BorderDisplay from "@/components/bits/BorderDisplay";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-8 bg-gray-900">
-      {/* <FloatingNavbar /> */}
-      {/* <GlassNavbar /> */}
-      <MorphingNavbar />
+      <BorderDisplay />
     </div>
   );
 }
